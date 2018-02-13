63d8823b1035cb6ac586d4ec695160376fbd0c09
Jeff Malone <jeff@6.1.0.159>
2018-02-13 15:24:18 +0100

    auto commit Tue Feb 13 15:24:18 CET 2018

--- bin/mrurxvt-generate-cache.sh
+++ bin/mrurxvt-generate-cache.sh
@@ -2,6 +2,7 @@
 
 set -e
 cd /tmp
-ln -fs $(which true) /tmp/urxvt
-export PATH=/tmp:$PATH
+#ln -fs $(which true) /tmp/urxvt
+#export PATH=/tmp:$PATH
+export MRURXVT_EXIT_EARLY=1
 nice -n 19 bash -c 'while :; do mrurxvt; sleep 1; done'
