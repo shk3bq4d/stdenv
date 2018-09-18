4e06242be5ac15d16ef46f1bf0db3dc24b6b4430
Jeff Malone <jeff@dec17>
2018-09-18 18:04:25 +0200

    auto commit Tue Sep 18 18:04:25 CEST 2018

--- bin/touchpad-toggle.sh
+++ bin/touchpad-toggle.sh
@@ -8,4 +8,4 @@ else
 	action="disable"
 fi
 echo $action > $f
-xinput list | sed -r -n -e '/ouch.ad/s/.*id=([0-9]+).*/\1/ p' | xargs --no-run-if-empty xinput --$action
+xinput list | sed -r -n -e '/ouch.ad/s/.*id=([0-9]+).*/\1/ p' | xargs -n 1 --no-run-if-empty xinput --$action
