bcd0e9ebd979d67389f034bdc35ba9089bd0f315
Jeff Malone <jeff@dec17>
2018-09-22 17:53:24 +0200

    auto commit Sat Sep 22 17:53:24 CEST 2018

---
+++ bin/at-work.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+# ex: set filetype=sh :
+##
+##Usage:  __SCRIPT__ REMOTEHOST [REMOTEPORT]
+##configures whatever action with whatever config
+##    REMOTEHOST: remote host where to ssh
+##    REMOTEPORT: JMX port (default: 12345)
+##
+## Author: Jeff Malone, 22 Sep 2018
+##
+
+set -euo pipefail
+
+set -x
+source ~/.std*_aliases
+source ~/bin/dot.hostname
+case $HOSTNAMEF in \
+$WORK_PC1F)
+	exit 0
+	;;
+dec17.ly.lan)
+	ips --no-internet | grep -q 'wlp58s0.*172.28'
+	;;
+esac
+exit 1
+# function usage() { sed -r -n -e s/__SCRIPT__/$(basename $0)/ -e '/^##/s/^..// p'   $0 ; }
+
+# [[ $# -eq 1 && ( $1 == -h || $1 == --help ) ]] && usage && exit 0
+
+# [[ $# -lt 1 || $# -gt 2 ]] && echo FATAL: incorrect number of args && usage && exit 1
+
+# for i in sed which grep; do ! command -v $i &>/dev/null && echo FATAL: unexisting dependency $i && exit 1; done
+
+# DIR="$( cd -P "$( dirname "$(readlink -f "${BASH_SOURCE[0]}")" )" && pwd )"
+
+# exec > >(tee -a ~/log/$(basename $0 .sh).log)
+# exec > >(tee >(logger --id=$$ -t "$(basename $0)" -p user.info ))
+# exec 2>&1
+
+# test -z "${HOSTNAMEF:-}" && HOSTNAMEF=$(hostname -f)
+
+echo EOF
+exit 0
+
