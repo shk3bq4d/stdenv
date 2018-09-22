e821831bede2f6a1afde18ee441796213798ce1d
Jeff Malone <jeff@dec17>
2018-09-22 15:12:38 +0200

    auto commit Sat Sep 22 15:12:38 CEST 2018

--- bin/i3-resize-window.py
+++ bin/i3-resize-window.py
@@ -1,74 +1,25 @@
 #!/usr/bin/env python3
 
-import sys
+#import mri3
 import i3ipc
-import mri3
-from pprint import pprint
+#from pprint import pprint
 
 i3c = i3ipc.Connection()
 focused = i3c.get_tree().find_focused()
-#print(focused.parent.type)
-#pprint(vars(focused.parent))
-mri3.debug(focused, recursive=False)
-#debug(focused.parent)
-#debug(focused.parent.nodes[0])
-sys.exit(0)
 
-def nb_screen():
-    visible_workspaces = [x['current_workspace'] for x in i3.get_outputs() if x['active'] and x['current_workspace']]
-    return len(visible_workspaces)
-focused = i3.get_tree().find_focused()
-#pprint(vars(focused))
-
-
-if focused.orientation == 'vertical':
-    container = focused
-elif focused.orientation == 'horizontal':
-    container = focused.parent
-elif focused.parent.orientation == 'vertical':
-    container = focused.parent
+jump = 1.3
+max_threshold = 0.85
+min_threshold = 0.1
+p = focused.percent
+n = p * jump
+if n > max_threshold:
+    n = p
+    while n / jump > min_threshold:
+        n = n / jump
+n = round(n * 100)
+print('{} => {}'.format(p, n))
+if focused.parent.orientation == 'vertical':
+    cA = (100, n)
 else:
-    container = focused
-idx = container.parent.nodes.index(container)
-if idx == len(container.parent.nodes) - 1:
-    #for i in range(len(container.parent.nodes)): container.command('move right')
-    container.command('move right')
-else:
-    #other_container = container.parent.nodes[len(container.parent.nodes) - 1]
-    other_container = container.parent.nodes[idx + 1]
-    if False:
-        cmd = 'swap container {} with {}'.format(container.id, other_container.id)
-        print(cmd)
-        pprint(i3.command(cmd))
-    if True:
-        container.command('focus')
-        cmd = 'swap container with con_id {}'.format(other_container.id)
-        print(cmd)
-        container.command(cmd)
-        focused.command('focus')
-
-#pprint(vars(container))
-sys.exit(0)
-if False:
-    for k, w in enumerate(wA):
-        if w.id in pH: continue
-        prev_border = w.border
-        if prev_border == 'pixel':
-            prev_border = u'{} {}'.format(prev_border, w.current_border_width)
-        pH[w.id] = prev_border
-
-next_border = 'normal'
-
-for w in wA:
-    w.command('border {}'.format(next_border))
-    pprint(vars(w))
-time.sleep(5)
-for k, w in enumerate(wA):
-    #w.command('border {}'.format(pA[k]))
-    w.command('border {}'.format('none'))
-#i3.command('[id="{}"] split toggle'.format(focused.window))
-#i3.command('[id="{}"] border pixel 10'.format(focused.window))
-#time.sleep(5)
-#i3.command('[id="{}"] border none'.format(focused.window))
-
-sys.exit(0)
+    cA = (n, 100)
+focused.command('resize set {} ppt {} ppt'.format(*cA))
