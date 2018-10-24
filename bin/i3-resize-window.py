1ee3bca0a864d66181bd746d28d82b59ca8e82a7
Jeff Malone <jeff@apr16>
2018-10-24 14:29:57 +0200

    auto commit Wed Oct 24 14:29:57 CEST 2018

--- bin/i3-resize-window.py
+++ bin/i3-resize-window.py
@@ -2,12 +2,15 @@
 
 #import mri3
 import i3ipc
+import sys
 #from pprint import pprint
 
 i3c = i3ipc.Connection()
 focused = i3c.get_tree().find_focused()
 
 jump = 1.3
+if len(sys.argv) > 1 and sys.argv[1] == '-':
+    jump = 1 / jump
 max_threshold = 0.85
 min_threshold = 0.1
 p = focused.percent
@@ -15,6 +18,10 @@ n = p * jump
 if n > max_threshold:
     n = p
     while n / jump > min_threshold:
+        n = n / jump
+elif n < min_threshold:
+    n = p
+    while n / jump < max_threshold:
         n = n / jump
 n = round(n * 100)
 print('{} => {}'.format(p, n))
