c2ddb0f513ccad7a032ed54874ae4d0926dc7e56
Jeff Malone <jeff@apr16>
2018-11-10 14:57:25 +0100

    auto commit Sat Nov 10 14:57:25 CET 2018

--- bin/mri3-toggle-orientation.py
+++ bin/mri3-toggle-orientation.py
@@ -10,10 +10,26 @@ except:
 import time
 
 def t(w, cmd):
+    pid = str(os.getpid())
+    fp = os.path.expanduser('~/.tmp/tmp/mri3-toggle-orientation-{}'.format(w.id))
+    try:
+        with open(fp, 'w') as f:
+            f.write(pid)
+    except BaseException as e:
+        pass
     w.command(cmd)
     w.command('border pixel 10')
+    
     time.sleep(5)
-    w.command('border none')
+    newpid = pid
+    try:
+        with open(fp, 'r') as f:
+            newpid = f.read()
+        if newpid == pid:
+            w.command('border none')
+            os.unlink(fp)
+    except:
+        w.command('border none')
 
 w = get_root().find_focused()
 p = w.parent
