d68951967e8dd8f4c4cd879e1455c018760bed40
Jeff Malone <jeff@6.1.0.159>
2018-12-04 09:57:41 +0100

    auto commit Tue Dec  4 09:57:41 CET 2018

---
+++ bin/yaml-pprint.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# /* ex: set filetype=python ts=4 sw=4 expandtab: */
+
+import os
+import sys
+import re
+import argparse
+import logging
+
+from pprint import pprint, pformat
+
+logger = logging.getLogger(__name__)
+
+def logging_conf(
+        level='INFO', # DEBUG
+        use='stdout' # "stdout syslog" "stdout syslog file"
+        ):
+    import logging.config
+    script_directory, script_name = os.path.split(__file__)
+    logging.config.dictConfig({'version':1,'disable_existing_loggers':False,
+       'formatters':{
+           'standard':{'format':'%(asctime)s %(levelname)-5s %(filename)s-%(funcName)s(): %(message)s'},
+           'syslogf': {'format':'%(filename)s[%(process)d]: %(levelname)-5s %(funcName)s(): %(message)s'},
+           #'graylogf':{"format":"%(asctime)s %(levelname)-5s %(filename)s-%(funcName)s(): %(message)s"},
+           },
+       'handlers':{
+           'stdout':   {'level':level,'formatter': 'standard','class':'logging.StreamHandler',         'stream': 'ext://sys.stdout'},
+           'file':     {'level':level,'formatter': 'standard','class':'logging.FileHandler',           'filename': os.path.expanduser('~/.tmp/log/{}.log'.format(os.path.splitext(script_name)[0]))}, #
+           'syslog':   {'level':level,'formatter': 'syslogf', 'class':'logging.handlers.SysLogHandler','address': '/dev/log', 'facility': 'user'}, # (localhost, 514), local5, ...
+           #'graylog': {'level':level,'formatter': 'graylogf','class':'pygelf.GelfTcpHandler',         'host': 'log.mydomain.local', 'port': 12201, 'include_extra_fields': True, 'debug': True, '_ide_script_name':script_name},
+       }, 'loggers':{'':{'handlers': use.split(),'level': level,'propagate':True}}})
+    try: logging.getLogger('sh.command').setLevel(logging.WARN)
+    except: pass
+
+def go(args):
+    # https://docs.python.org/2/library/argparse.html
+    # logger.info(__file__)
+    # logger.debug(__file__)
+    if 'VIMRUNTIME' in os.environ: args = ['HEHE', '-i']
+    parser = argparse.ArgumentParser(description="Python pprint a yaml file")
+    parser.add_argument("FILENAME", type=str, nargs='+', help="file to process")
+    ar = parser.parse_args(args)
+    for k, fn in enumerate(ar.FILENAME):
+        if k != 0:
+            print('---')
+        with open(fn, 'rb') as f:
+            pprint(yaml.load(f))
+    # pprint(ar)
+
+if __name__ == '__main__':
+
+    logging_conf()
+    try:
+        go(sys.argv[1:])
+    except BaseException as e:
+        #logging.exception('oups for %s', sys.argv)
+        logging.error('oups for %s', sys.argv)
+        raise type(e), type(e)(e), sys.exc_info()[2]
+
