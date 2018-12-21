0a5f4fc7ba1a8aed1a861b21c243f6d7f9acbf08
Jeff Malone <jeff@apr16>
2018-12-21 10:40:38 +0100

    auto commit Fri Dec 21 10:40:38 CET 2018

---
+++ bin/random-file-in-cwd.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# ex: set filetype=sh :
+##
+##Usage:  __SCRIPT__ REMOTEHOST [REMOTEPORT]
+##configures whatever action with whatever config
+##    REMOTEHOST: remote host where to ssh
+##    REMOTEPORT: JMX port (default: 12345)
+##
+## Author: Jeff Malone, 21 Dec 2018
+##
+
+find -maxdepth 1 -type f -print0 | shuf -z | head -zn 1 | xargs -0 basename
