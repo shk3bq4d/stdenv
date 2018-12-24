d7436660e4ec61f4fbe18403105e6c24b0c2f352
Jeff Malone <jeff@apr16>
2018-12-24 10:18:17 +0100

    auto commit Mon Dec 24 10:18:17 CET 2018

---
+++ bin/tailf-daily-rotating.sh
@@ -0,0 +1,84 @@
+#!/usr/bin/env bash
+# ex: set filetype=sh :
+##
+##Usage:  __SCRIPT__ REMOTEHOST [REMOTEPORT]
+##configures whatever action with whatever config
+##    REMOTEHOST: remote host where to ssh
+##    REMOTEPORT: JMX port (default: 12345)
+##
+## Author: Jeff Malone, 24 Dec 2018
+##
+
+set -euo pipefail
+
+# function usage() { sed -r -n -e s/__SCRIPT__/$(basename $0)/ -e '/^##/s/^..// p'   $0 ; }
+
+# [[ $# -eq 1 && ( $1 == -h || $1 == --help ) ]] && usage && exit 0
+
+# [[ $# -lt 1 || $# -gt 2 ]] && echo FATAL: incorrect number of args && usage && exit 1
+
+# for i in sed which grep; do ! command -v $i &>/dev/null && echo FATAL: unexisting dependency $i && exit 1; done
+
+# DIR="$( cd -P "$( dirname "$(readlink -f "${BASH_SOURCE[0]}")" )" && pwd )"
+
+# exec > >(tee -a ~/log/$(basename $0 .sh).log)
+# exec > >(tee >(logger --id=$$ -t "$(basename $0)" -p user.info ))
+# exec 2>&1
+
+# test -z "${HOSTNAMEF:-}" && HOSTNAMEF=$(hostname -f)
+
+DATEBIN=date
+uname | grep -qi freebsd && DATEBIN=gdate
+! hash $DATEBIN 2>/dev/null && echo "FATAL: not present $DATEBIN. pkg install coreutils" && exit 1
+
+if [[ -n $VIMF6 ]]; then
+	filepath="./BrowserServer.2018-12-24"
+else
+	filepath="$1"
+	if [[ ! -f "$filepath" ]]; then
+		echo "FATAL not a file $f"
+		exit 1
+	fi
+fi
+filename="$(basename "$filepath")"
+
+
+for format in  \
+	'%Y%m%d'  \
+	'%Y.%m.%d'  \
+	'%Y-%m-%d'  \
+	'%y%m%d'  \
+	'%y.%m.%d'  \
+	'%y-%m-%d'  \
+	'%d.%m.%Y' \
+	'%d-%m-%Y' \
+	'%d%m%Y' \
+	'%d.%m.%y' \
+	'%d-%m-%y' \
+	'%d%m%y' \
+	; do
+	if [[ -f "$filepath" ]]; then
+		today=$(date "+$format" -r "$filepath")
+	else
+		today=$(date "+$format")
+	fi
+	if echo "$filename" | grep -Fq $today; then
+		template=${filepath//$today/}
+		echo "detected format is $format ! template is $template"
+		while :; do
+			todays_filepath="$template$(date +"$format")"
+			now=$(date +%s)
+			midnight=$(date +%s -d 'tomorrow 0:00')
+			diff=$(($midnight - $now))
+			echo "diff is $diff"
+			echo "timeout $diff tail -n 300 -F \"$todays_filepath\""
+			timeout $diff tail -n 300 -F "$todays_filepath"
+		done
+		break
+	fi
+done
+
+
+echo EOF
+exit 0
+
