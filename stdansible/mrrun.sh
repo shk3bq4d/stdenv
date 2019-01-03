711953223bd14e31e4b73182d90bfcd4b8cfc677
Jeff Malone <jeff@6.1.0.159>
2019-01-03 10:02:37 +0100

    auto commit Thu Jan  3 10:02:37 CET 2019

--- stdansible/mrrun.sh
+++ stdansible/mrrun.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
-ansible-playbook *.yml --ask-become-pass --diff -l 127.0.0.1
+if [[ $# -eq 0 ]]; then
+	set -x
+	ansible-playbook *.yml --ask-become-pass --diff -l 127.0.0.1
+	set +x
+else
+	set -x
+	ansible-playbook "$@" --ask-become-pass --diff -l 127.0.0.1
+	set +x
+fi
