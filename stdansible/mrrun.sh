216df6269a3722508055d852ed6d54442a51839b
Jeff Malone <jeff@apr16>
2019-01-06 20:38:02 +0100

    auto commit Sun Jan  6 20:38:02 CET 2019

--- stdansible/mrrun.sh
+++ stdansible/mrrun.sh
@@ -5,7 +5,11 @@ if [[ $# -eq 0 ]]; then
 	ansible-playbook *.yml --ask-become-pass --diff -l 127.0.0.1
 	set +x
 else
-	set -x
-	ansible-playbook "$@" --ask-become-pass --diff -l 127.0.0.1
-	set +x
+	for pb in "$@"; do
+		if grep -wq become $pb &>/dev/null; then
+			ansible-playbook $pb --ask-become-pass --diff -l 127.0.0.1
+		else
+			ansible-playbook $pb                   --diff -l 127.0.0.1
+		fi
+	done
 fi
