68cd02d9dad92b49d4120a33cd4542962e5ee479
Jeff Malone <jeff@may19>
2019-06-04 08:04:53 +0200

    auto commit Tue Jun  4 08:04:53 CEST 2019

---
+++ py/mr_sendkeys.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# /* ex: set filetype=python ts=4 sw=4 expandtab: */
+
+from sh import xdotool
+import os
+import sys
+import re
+import unittest
+import argparse
+import logging
+
+from pprint import pprint, pformat
+
+logger = logging.getLogger(__name__)
+
+class MrSendkeysTest(unittest.TestCase):
+    #def __init__(self, methodName='runTest'): pass
+    def tearDown(self): pass
+    def tearUp(self): pass
+
+    @classmethod
+    def setUpClass(cls): pass
+
+    @classmethod
+    def tearDownClass(cls): pass
+
+    def test_queue_summary_alter(self):
+        self.assertIn(   'a', 'abcde', msg='a is supposed to be in abcde')
+        self.assertNotIn('z', 'abcde', msg='z is not supposed to be in abcde')
+        self.assertNotEqual('expected', 'actual', msg='expected is first argument')
+        # assertAlmostEqual assertAlmostEquals assertDictContainsSubset assertDictEqual
+        # assertEqual assertEquals assertFalse assertGreater assertGreaterEqual
+        # assertIn assertIs assertIsInstance assertIsNone assertIsNot assertIsNotNone
+        # assertItemsEqual assertLess assertLessEqual assertListEqual assertMultiLineEqual
+        # assertNotAlmostEqual assertNotAlmostEquals assertNotEqual assertNotEquals
+        # assertNotIn assertNotIsInstance assertNotRegexpMatches assertRaises
+        # assertRaisesRegexp assertRegexpMatches assertSequenceEqual assertSetEqual
+        # assertTrue assertTupleEqual assert_ countTestCases debug defaultTestResult
+        # doCleanups fail failIf failIfAlmostEqual failIfEqual failUnless failUnlessAlmostEqual
+        # failUnlessEqual failUnlessRaises failureException id longMessage maxDiff
+        # run setUp setUpClass shortDescription skipTest tearDown tearDownClass
+
+def logging_conf(
+        level='INFO', # DEBUG
+        use='stdout' # "stdout syslog" "stdout syslog file"
+        ):
+    import logging.config
+    script_directory, script_name = os.path.split(__file__)
+    logging.config.dictConfig({'version':1,'disable_existing_loggers':False,
+       'formatters':{
+           'standard':{'format':'%(asctime)s %(levelname)-5s %(filename)s-%(funcName)s(): %(message)s'},
+           'syslogf': {'format':'%(filename)s[%(process)d]: %(levelname)-5s %(funcName)s(): %(message)s'},
+           #'graylogf':{"format":"%(asctime)s %(levelname)-5s %(filename)s-%(funcName)s(): %(message)s"},
+           },
+       'handlers':{
+           'stdout':   {'level':level,'formatter': 'standard','class':'logging.StreamHandler',         'stream': 'ext://sys.stdout'},
+           'file':     {'level':level,'formatter': 'standard','class':'logging.FileHandler',           'filename': os.path.expanduser('~/.tmp/log/{}.log'.format(os.path.splitext(script_name)[0]))}, #
+           'syslog':   {'level':level,'formatter': 'syslogf', 'class':'logging.handlers.SysLogHandler','address': '/dev/log', 'facility': 'user'}, # (localhost, 514), local5, ...
+           #'graylog': {'level':level,'formatter': 'graylogf','class':'pygelf.GelfTcpHandler',         'host': 'log.mydomain.local', 'port': 12201, 'include_extra_fields': True, 'debug': True, '_ide_script_name':script_name},
+       }, 'loggers':{'':{'handlers': use.split(),'level': level,'propagate':True}}})
+    try: logging.getLogger('sh.command').setLevel(logging.WARN)
+    except: pass
+
+def find_window():
+    print("Click on your window")
+    sys.stdout.flush()
+    i = str(xdotool('selectwindow')).strip()
+    print("window id is {}".format(i))
+    return i
+
+def go(args):
+    parser = argparse.ArgumentParser(description="Send keys to a specific window")
+    parser.add_argument("ARGS", type=str, nargs='+', help="text to type")
+    parser.add_argument("-i", "--id", nargs='?', help="window ID where to send the key event", type=str)
+    ar = parser.parse_args(args)
+    window_id = find_window() if ar.id is None else ar.id
+    if len(args) > 0:
+        args = ' '.join(args)
+        for i in args:
+            bA = 'type --clearmodifiers --window {}'.format(window_id).split()
+            bA.append(i)
+            xdotool(*bA)
+
+if __name__ == '__main__':
+    logging_conf()
+    if 'VIMF6' in os.environ and False:
+        unittest.main()
+    else:
+        try:
+            go(sys.argv[1:])
+        except BaseException as e:
+            logging.exception('oups for %s', sys.argv)
+            #logging.error('oups for %s', sys.argv)
+            #raise type(e), type(e)(e), sys.exc_info()[2]
+
