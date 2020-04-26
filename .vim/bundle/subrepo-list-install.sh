7081c4a44ecc40dd16a4f799f9f35075746221d0
Jeff Malone <jeff@dec17>
2020-04-26 14:41:38 +0200

    auto commit Sun Apr 26 14:41:38 CEST 2020

--- .vim/bundle/subrepo-list-install.sh
+++ .vim/bundle/subrepo-list-install.sh
@@ -5,4 +5,5 @@ cd -P "$( dirname "${BASH_SOURCE[0]}" )"
 cat subrepo-list.txt | while read dir url; do
 	[[ -d "$dir" ]] && continue
 	git clone $url "$dir"
+	git submodule init && git submodule update || true
 done
