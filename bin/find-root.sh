d2d7d46fd9816d4162d4b9d84eb36faf6685cc81
Jeff Malone <jeff@apr16>
2020-05-02 11:21:15 +0200

    auto commit Sat May  2 11:21:15 CEST 2020

---
+++ bin/find-root.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# ex: set filetype=sh fenc=utf-8 expandtab ts=4 sw=4 :
+##
+##Usage:  __SCRIPT__ REMOTEHOST [REMOTEPORT]
+##configures whatever action with whatever config
+##    REMOTEHOST: remote host where to ssh
+##    REMOTEPORT: JMX port (default: 12345)
+##
+## Author: Jeff Malone, 02 May 2020
+##
+
+set -euo pipefail
+
+set -x
+find $(ls -1a / | grep -xEv 'proc|sys|lost\+found' | sed -r -e 's,^,/,') "$@"
