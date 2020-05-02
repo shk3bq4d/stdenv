d5003742b346e29b30142e47bedb1f1464b5a46d
Jeff Malone <jeff@dec17>
2020-05-02 09:45:14 +0200

    auto commit Sat May  2 09:45:14 CEST 2020

---
+++ bin/docker-stdenv-user.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+docker run "$@" \
+	-e STDENV_DEBUG= \
+	-e STDENV_USER_SUDOER=1 \
+	-e STDENV_USER_UID=$(id -u) \
+	-e STDENV_USER_GID=$(id -g) \
+	-e STDENV_USER_NAME=$(id -un) \
+	-e STDENV_USER_GROUPNAME=$(id -gn) \
+	-e STDENV_RUNAS=$(id -un) \
+	-it shk3bq4d/stdenv:stdenv
