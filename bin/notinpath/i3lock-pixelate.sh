85c6fa97019fdc1376e316883414fc3e338068aa
Jeff Malone <jeff@may19>
2020-09-17 17:30:41 +0200

    auto commit Thu Sep 17 17:30:41 CEST 2020

--- bin/notinpath/i3lock-pixelate.sh
+++ bin/notinpath/i3lock-pixelate.sh
@@ -2,15 +2,42 @@
 
 icon="$HOME/.xlock/icon.png"
 tmpbg='/tmp/screen.png'
+tmpbg='/tmp/screen.jpg'
 
 (( $# )) && { icon=$1; }
 
-i3lock -c 000000 -n &
-FOO_PID=$!
-scrot "$tmpbg"
-convert "$tmpbg" -scale 5% -scale 2000% "$tmpbg"
+set -x
+if true; then
+	i3lock -c 000000 -n &
+	FOO_PID=$!
+fi
+
+i=$RANDOM
+j=$(( $i % 10 ))
+case $j in \
+[0-1]) a=1; b=10000;;
+[2-3]) a=2; b=5000 ;;
+[4-5]) a=4; b=2500 ;;
+[6-7]) a=5; b=2000 ;;
+[8])   a=8; b=1205 ;;
+[9])   a=10; b=1000;;
+esac
+#a=100; b=100;
+q=$(( $i % 37 ))
+scrot --quality $q "$tmpbg"
+ls -l "$tmpbg"
+if [[ "$tmpbg" != *".png" ]]; then
+	newbg="${tmpbg}.png"
+	#convert "$tmpbg" "$newbg"
+	convert "$tmpbg" -scale ${a}% -scale ${b}% "$newbg"
+	#convert "$tmpbg" "$newbg"
+	tmpbg="$newbg"
+fi
+
+#convert "$tmpbg" -scale 2% -scale 5000% "$tmpbg"
+#convert "$tmpbg" -scale 2% -scale 5000% "$tmpbg"
 #convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
 #convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
-kill $FOO_PID
+test -n "$FOO_PID" && kill $FOO_PID
 i3lock -enf -i "$tmpbg"
 
--- doc/magic-values-constant.md
+++ doc/magic-values-constant.md
@@ -1,3 +1,8 @@
+31536000 # seconds in 365 days
+1382400 # seconds in 16 days
+1209600 # seconds in 14 days
+2592000 # seconds in 30 days
+604800 # seconds in 7 days
 86400 # seconds in a day
 8760  # hours in a 365-year
 1440  # minutes in a day
