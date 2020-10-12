90e9784750cf8195bf3e7597269e604d5bb8465b
Jeff Malone <jeff@dec17>
2020-10-12 19:43:49 +0200

    auto commit Mon Oct 12 19:43:49 CEST 2020

--- bin/netflix-nf.sh
+++ bin/netflix-nf.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 ~/bin/mute-off.sh
+~/bin/volume-100.sh
 nohup /usr/bin/google-chrome https://www.netflix.com/browse &>/dev/null &
 for i in $(seq 5); do
 	/usr/bin/mplayer ~/Music/bip-stereo.mp3
