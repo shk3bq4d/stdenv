cc98e84b9c7edde5aa636efb9068825372614af3
Jeff Malone <jeff@nov20.ly.lan>
2020-11-14 08:58:09 +0100

    patch stdhome-dirname.sh

--- bin/stdhome-dirname.sh
+++ bin/stdhome-dirname.sh
@@ -5,10 +5,27 @@ set -e
 if [[ -n "$STDHOME_DIRNAME" ]]; then
 	echo "$STDHOME_DIRNAME"
 else
-	if ! type mrdirname 2>/dev/null; then
-		function mrdirname() {
-			dirname "$@"
-		}
-	fi
-	cd -P "$(mrdirname "$(mrdirname "$( readlink -f "${BASH_SOURCE[0]}" )")")" &>/dev/null && pwd
+	test -z "${USER:-}" && USER=$(whoami)
+	for i in \
+		~/git/$USER/stdhome \
+		~/git/ly.abc1.ch/stdhome \
+		~/git/github/shk3bq4d/stdhome \
+		~/git/shk3bq4d/stdhome \
+	; do
+		[[ -d "$i" ]] && echo "$i" && exit 0
+	done
+	for i in \
+		~/git/$USER/stdenv \
+		~/git/ly.abc1.ch/stdenv \
+		~/git/github/shk3bq4d/stdenv \
+		~/git/shk3bq4d/stdenv \
+	; do
+		[[ -d "$i" ]] && echo "$(dirname "$i")/stdhome" && exit 0
+	done
+	find ~ -type d -name stdhome -or -name stdenv -print -exit | while read i; do
+		echo "$(dirname "$i")/stdhome"
+		exit 0
+	done
+	>&2 echo "stdhome-dirname.sh not found"
+	exit 1
 fi
