4d0572f44222dcb6142e5ca102ae8392988f8122
Jeff Malone <jeff@nov20.ly.lan>
2021-07-22 12:01:49 +0200

    auto commit 2021-07-22T12:01:49 CEST

--- bin/kubectl-letsencrypt.sh
+++ bin/kubectl-letsencrypt.sh
@@ -9,7 +9,13 @@ kubectl get -A --show-kind=true ingresses.networking.k8s.io "$@"
 kubectl get -A --show-kind=true orders.acme.cert-manager.io "$@" | grep -E --color=always "^|invalid"
 kubectl get -A --show-kind=true certificates.cert-manager.io "$@"| grep -E --color=always "^|True"
 kubectl get -A --show-kind=true certificaterequests.cert-manager.io "$@"| grep -E --color=always "^|True"
-kubectl get -A --show-kind=true certificatesigningrequests.certificates.k8s.io "$@"| grep -E --color=always "^|True"
+kubectl get -A certificates.cert-manager.io -o jsonpath='{range .items[*]}{.metadata.namespace} {.metadata.name} {.spec.secretName}{"\n"}{end}' | while read namespace name secretname; do
+    printf '%-20s certificates.cert-manager.io/%-85s ' $namespace $name
+    set +e
+    ! { kubectl get -n $namespace secret $secretname -o jsonpath="{.data.tls\\.crt}" | base64 -d | openssl-cert-info.sh 2>/dev/null | grep notAfter; } && echo ""
+    set -e
+done
+kubectl get -A --show-kind=true certificatesigningrequests.certificates.k8s.io "$@"| grep -E --color=always "^|True" || true
 
 echo "
 kubectl delete certificaterequest XYZ
