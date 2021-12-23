f3e36392510492ce19400c44ecbc715ef265f279
Jeff Malone <jeff@apr16>
2021-12-23 08:55:04 +0100

    auto commit 2021-12-23T08:55:04 CET

--- bin/chmod-world-readable.sh
+++ bin/chmod-world-readable.sh
@@ -16,19 +16,44 @@ for var in "$@"; do
     fi
 done
 
+# set +e
+# echo ">>>>"
+# {
+#     find "$@" -type d -! -perm -o+rx -print -quit 2>&1 || true
+#     find "$@" -type f -! -perm -o+r  -print -quit 2>&1 || true
+# } | grep -E "^" && echo yes || echo no
+# echo "<<<<"
+# set -e
+{
+    find "$@" -type d -! -perm -o+rx -print -quit 2>&1 || true
+    find "$@" -type f -! -perm -o+r  -print -quit 2>&1 || true
+} | grep -qE "^" || { echo "No unreadable or accessible file or folder" && exit 0; }
+
 if [[ $EUID -ne 0 ]]; then
     if ! find "$@" -not -uid $EUID &>/dev/null ||
         find "$@" -not -uid $EUID | grep -qE ^; then
         echo "one ore more files not owned by $EUID, restarting script with sudo access"
-        sudo $0 "$@"
+        sudo bash $0 "$@"
         exit $?
     fi
 fi
 
-BACKUP_PERMS="./$(basename "$0" .sh).backup.$(date +%s)"
-find "$@" -print0 | xargs -0 ls -ld > $BACKUP_PERMS
-test -n "${SUDO_UID:-}" && chown $SUDO_UID $BACKUP_PERMS
-test -n "${SUDO_GID:-}" && chgrp $SUDO_GID $BACKUP_PERMS
+if [[ -n "${SUDO_USER:-}" ]]; then
+    _HOME="$(eval echo ~${SUDO_USER})"
+else
+    _HOME="$HOME"
+fi
+BACKUP_PERMS="$_HOME/$(basename "$0" .sh).backup.$(date +%s)"
+echo -n "Would you like to save a copy of the perms in $BACKUP_PERMS (Yn): " # read
+read _read
+echo ""
+case "${_read,,}" in \
+""|y|yes)
+    find "$@" -print0 | xargs -0 ls -ld > $BACKUP_PERMS
+    test -n "${SUDO_UID:-}" && chown $SUDO_UID $BACKUP_PERMS
+    test -n "${SUDO_GID:-}" && chgrp $SUDO_GID $BACKUP_PERMS
+    ;;
+esac # read
 
 find "$@" -type d -! -perm -o+rx -print0 | xargs -rt0 chmod o+rx
 find "$@" -type f -! -perm -o+r  -print0 | xargs -rt0 chmod o+r
