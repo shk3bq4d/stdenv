2c3708e2fc35c1ea90878047eac2453952e541d3
Jeff Malone <jeff@may19>
2022-03-17 16:35:02 +0100

    auto commit 2022-03-17T16:35:02 CET

---
+++ bin/sed-emails.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# ex: set filetype=sh fenc=utf-8 expandtab ts=4 sw=4 :
+
+set -euo pipefail
+umask 027
+export PATH=/usr/local/sbin:/sbin:/usr/local/bin:/bin:/usr/sbin:/usr/bin:~/bin
+
+
+cat "$@" |
+sed -r -n -e \
+    's,([a-zA-Z0-9_.-]+@[a-zA-Z0-9_.-]+),\n\1\n,g p' |
+sed -r -n -e \
+      '/[a-zA-Z0-9_.-]+@[a-zA-Z0-9_.-]+/ p'
+
+exit 0
+    's,(?:[a-z0-9!#$%&'"'"'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'"'"'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\]),hehe, p'
