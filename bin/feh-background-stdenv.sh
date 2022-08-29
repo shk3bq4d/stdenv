cc92ce92e857e9d7f2b1afd584598d7b96947e22
Jeff Malone <jeff@apr16>
2022-08-29 08:47:52 +0200

    auto commit 2022-08-29T08:47:52 CEST

--- bin/feh-background-stdenv.sh
+++ bin/feh-background-stdenv.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 # ex: set filetype=sh fenc=utf-8 expandtab ts=4 sw=4 :
 #
+
+case $(hostname -f) in \
+feb22*)
+    exit 0
+    ;;
+esac
 nohup feh --bg-scale ~/Pictures/i3lock/boreal3.png </dev/null &>/dev/null &
 #feh --bg-scale ~/Pictures/i3lock/boreal3.png
