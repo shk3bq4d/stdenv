3edd9c2c1a0abe6f9cba81cd2fe6ba814d3a2266
Jeff Malone <jeff@apr16>
2022-09-14 17:18:49 +0200

    auto commit 2022-09-14T17:18:49 CEST

---
+++ bin/yaml-to-json.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# ex: set filetype=sh fenc=utf-8 expandtab ts=4 sw=4 :
+
+set -euo pipefail
+umask 027
+export PATH=/usr/local/sbin:/sbin:/usr/local/bin:/bin:/usr/sbin:/usr/bin:~/bin
+
+if [[ $# -eq 0 ]]; then
+    yq --output-format=json e . -
+else
+    yq --output-format=json e . "$@"
+fi
