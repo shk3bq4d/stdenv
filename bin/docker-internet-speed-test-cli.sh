edc61a68a48fdda2a96dd3d8acf8b3d83f9622b9
Jeff Malone <jeff@apr16>
2022-09-28 10:24:56 +0200

    auto commit 2022-09-28T10:24:56 CEST

---
+++ bin/docker-internet-speed-test-cli.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# ex: set filetype=sh fenc=utf-8 expandtab ts=4 sw=4 :
+
+set -euo pipefail
+umask 027
+export PATH=/usr/local/sbin:/sbin:/usr/local/bin:/bin:/usr/sbin:/usr/bin:~/bin
+
+# https://hub.docker.com/r/gists/speedtest-cli
+# https://github.com/vgist/dockerfiles/tree/master/speedtest-cli
+docker run --rm gists/speedtest-cli
+exit 0
