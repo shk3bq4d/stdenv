02e9303c1c8de45fafe9416c6db3c0ba47bc27a4
Jeff Malone <jeff@feb22>
2022-12-04 10:56:52 +0100

    auto commit 2022-12-04T10:56:52 CET

--- bin/ansible-jinja-cli.sh
+++ bin/ansible-jinja-cli.sh
@@ -17,10 +17,15 @@ function usage() { sed -r -n -e "s/__SCRIPT__/$(basename $0)/" -e '/^##/s/^..// 
 
 [[ $# -lt 1 || $# -gt 1 ]] && echo "FATAL: incorrect number of args" && usage && exit 1
 
+! test -f "$1" && echo "FATAL: not a file $1" && exit 1
+
 [[ -t 1 ]] && export ANSIBLE_FORCE_COLOR=true
 
 _tempdir=$(mktemp -d); function cleanup() { [[ -n "${_tempdir:-}" ]]  && [[ -d "$_tempdir" ]]  && rm -rf "$_tempdir"  || true; }; trap 'cleanup' SIGHUP SIGINT SIGQUIT SIGTERM EXIT
-if ! ansible all -i "localhost," -m template -a "src='$1' dest=$_tempdir/out" --connection=local &>$_tempdir/err; then
+cp "$1" "$_tempdir/in"
+f=~/git/sf/dcn/iaac-master/ans/roles/sf-zabbix-template/templates/macros.j2
+test -f "$f" && cp "$f" "$_tempdir"
+if ! ansible all -i "localhost," -m template -a "src='$_tempdir/in' dest=$_tempdir/out" --connection=local -e zabbix_version=6.2.5 -e group_template_uuid=abcd -e _template_uuid=abcd -e template_name=ansible-jinja-cli &>$_tempdir/err; then
     cat $_tempdir/err
     exit 1
 fi
