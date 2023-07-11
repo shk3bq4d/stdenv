d0217eb70077d5c4a9d9c7783f0177ab0dc96c5e
Jeff Malone <jeff@may19>
2023-07-11 17:59:36 +0200

    auto commit 2023-07-11T17:59:36 CEST

---
+++ bin/sed_unix_timestamps_to_human.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# 1000000000 => 2001-09-09 03:46:40
+# 2000000000 => 2033-05-18 05:33:20
+
+bip() {
+	cat << 'EOF'
+msg=audit(1689087243.868:61): avc:  denied  { getatk
+other line
+EOF
+}
+# https://mywiki.wooledge.org/IFS#:~:text=The%20IFS%20variable%20is%20used,is%20space%2C%20tab%2C%20newline.
+delimiter=","
+delimiter=$'\1'
+
+#bip |
+	sed -r -e 's/(.*)(\<1[0-9]{9}\>)(.*)/\1'"$delimiter"'\2'"$delimiter"'\3/' \
+	"$@" |
+	while IFS="$delimiter" read a b c; do
+		if [[ -n "$b" ]]; then
+			echo "$(date -d @$b +'%Y.%m.%d %H:%M:%S') $a $c"
+		else
+			echo "$a"
+		fi
+	done
