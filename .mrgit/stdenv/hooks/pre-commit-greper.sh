7f3d43c4ffa3c7b62bcca91de748a00f8638b676
Jeff Malone <jeff@apr16>
2023-08-14 06:48:17 +0200

    auto commit 2023-08-14T06:48:17 CEST

--- .mrgit/stdenv/hooks/pre-commit-greper.sh
+++ .mrgit/stdenv/hooks/pre-commit-greper.sh
@@ -1,12 +1,5 @@
-#!/usr/bin/env bash                                            
+#!/usr/bin/env bash
 # ex: set filetype=sh :
-##
-##Usage:  __SCRIPT__ REMOTEHOST [REMOTEPORT]
-##configures whatever action with whatever config
-##    REMOTEHOST: remote host where to ssh
-##    REMOTEPORT: JMX port (default: 12345)
-##
-##
 
 set -euo pipefail
 a="\\<"
@@ -16,6 +9,15 @@ word1=$'\x72\x75\x6d\x6f'
 word2=$'\x6b\x73'
 word3=$'\x6b\x75\x64\x65\x6c\x73'
 word4=$'\x63\x61\x72\x74\x69\x65\x72'
+if [[ -n ${VIMF6:-} ]]; then
+cat << EOF
+word1 is $word1
+word2 is $word2
+word3 is $word3
+word4 is $word4
+EOF
+	exit 0
+fi
 grep --color --with-filename -n -i \
 	-e ${a}${word1}${b} \
 	-e ${a}m${word1}${b} \
@@ -24,5 +26,5 @@ grep --color --with-filename -n -i \
 	-e ${a}${word2}${p}dev${p}local${b} \
 	-e ${word3} \
 	-e ${word4} \
-	"$@" 
+	"$@"
 exit $?
