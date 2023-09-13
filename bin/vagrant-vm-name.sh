61cc8bf552a938b384e6becb60c1d166b4508f8e
Jeff Malone <jeff@feb22>
2023-09-13 09:31:32 +0200

    auto commit 2023-09-13T09:31:32 CEST

---
+++ bin/vagrant-vm-name.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# ex: set filetype=sh fenc=utf-8 expandtab ts=4 sw=4 :
+
+set -euo pipefail
+umask 027
+export PATH=/usr/local/sbin:/sbin:/usr/local/bin:/bin:/usr/sbin:/usr/bin:~/bin
+
+vagrant status --debug |&
+  grep Name: |
+  awk-print-last.sh |
+  head -n 1
+
+exit 0
---
+++ doc/stack-sf.md
@@ -0,0 +1,12 @@
+* Gerald decom
+* script gc-host-delete
+* beats command not found
+* shane max upload size
+* shane max time execution
+* srl py branch may19
+* Okta Nicky
+* Interview Questions Ansible RH
+* deskpro jeaddom
+* patch tuesday windows OOS
+* yum sync
+* mdatp update
