d91a632068b7452c8fcbaef3f14de74661dda002
Jeff Malone <jeff@feb22>
2024-01-30 10:09:48 +0100

    auto commit 2024-01-30T10:09:48 CET

--- javascript/bookmarklets/html2clipboard.js
+++ javascript/bookmarklets/html2clipboard.js
@@ -9,4 +9,21 @@
 /* globals alert, document, location, prompt, URL, Blob, fetch, navigator, $ */
 "use strict";
 
-(new Function("var copy_html_to_clipboard = async function() { try {console.log('trying to copy, ensure page is focused'); await navigator.clipboard.writeText(document.documentElement.outerHTML);console.log('success!');}catch (e) {console.log(e); console.log('try to focus page'); setTimeout(arguments.callee, 1000);}}; setTimeout(copy_html_to_clipboard, 0);"))();
+var k,bA;
+
+bA = document.getElementsByTagName('SCRIPT');
+for (k = bA.length - 1; k >= 0; --k)
+{   
+    bA[k].src = bA[k].innerHTML = '';
+}
+
+bA = document.getElementsByTagName('STYLE');
+for (k = bA.length - 1; k >= 0; --k)
+{   
+    if (bA[k] && bA[k].parentNode) bA[k].parentNode.removeChild(bA[k]);
+}
+
+(
+// async function do not pass the yuicompressor
+new Function("var copy_html_to_clipboard = async function() { try {console.log('trying to copy, ensure page is focused'); await navigator.clipboard.writeText(document.documentElement.outerHTML);console.log('success!');}catch (e) {console.log(e); console.log('try to focus page'); setTimeout(arguments.callee, 1000);}}; setTimeout(copy_html_to_clipboard, 0);")
+)();
