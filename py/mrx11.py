2fe58ac81dea67dd29e3c981f95198e17b28e0aa
Jeff Malone <jeff@nov20>
2024-03-24 11:02:14 +0100

    auto commit 2024-03-24T11:02:14 CET

---
+++ py/mrx11.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# /* ex: set filetype=python ts=4 sw=4 expandtab: */
+
+from Xlib import X, display
+import os
+import sys
+import re
+import unittest
+import argparse
+import logging
+from typing import Sequence, Union, Iterator, List, Tuple, no_type_check, Any, Optional
+
+from pprint import pprint, pformat
+
+os.umask (0o27)
+logger = logging.getLogger(__name__)
+
+def logging_conf(
+        level='INFO', # DEBUG
+        use='stdout', # "stdout syslog" "stdout syslog file"
+        filepath=None,
+        ) -> None:
+    import logging.config
+    script_directory, script_name = os.path.split(__file__)
+    # logging.getLogger('sh.command').setLevel(logging.WARN)
+    if filepath is None:
+        filepath = os.path.expanduser('~/.tmp/log/{}.log'.format(os.path.splitext(script_name)[0]))
+    logging.config.dictConfig({'version':1,'disable_existing_loggers':False,
+       'formatters':{
+           'standard':{'format':'%(asctime)s %(levelname)-5s %(filename)s-%(funcName)s(): %(message)s'},
+           'syslogf': {'format':'%(filename)s[%(process)d]: %(levelname)-5s %(funcName)s(): %(message)s'},
+           #'graylogf':{"format":"%(asctime)s %(levelname)-5s %(filename)s-%(funcName)s(): %(message)s"},
+           },
+       'handlers':{
+           'stdout':   {'level':level,'formatter': 'standard','class':'logging.StreamHandler',         'stream': 'ext://sys.stdout'},
+           'file':     {'level':level,'formatter': 'standard','class':'logging.FileHandler',           'filename': filepath}, #
+           'syslog':   {'level':level,'formatter': 'syslogf', 'class':'logging.handlers.SysLogHandler','address': '/dev/log', 'facility': 'user'}, # (localhost, 514), local5, ...
+           #'graylog': {'level':level,'formatter': 'graylogf','class':'pygelf.GelfTcpHandler',         'host': 'log.mydomain.local', 'port': 12201, 'include_extra_fields': True, 'debug': True, '_ide_script_name':script_name},
+       }, 'loggers':{'':{'handlers': use.split(),'level': level,'propagate':True}}})
+    try: logging.getLogger('sh.command').setLevel(logging.WARN)
+    except: pass
+
+def go(args: List[str]) -> int:
+    list_modes()
+
+def list_modes():
+    d = display.Display()
+    screen = d.screen()
+    root = screen.root
+
+    res = root.xrandr_get_screen_resources()
+    for mode in res.modes:
+        print(f"Mode ID: {mode.id}, Width: {mode.width}, Height: {mode.height}")
+
+    d.close()
+
+if __name__ == '__main__':
+    logging_conf()
+    if 'VIMF6' in os.environ and False:
+        unittest.main()
+    else:
+        try:
+            r = go(sys.argv[1:])
+        except BaseException as e:
+            logger.exception('oups for %s', sys.argv)
+            sys.exit(1)
+        sys.exit(r)
+
