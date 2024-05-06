05ce02e4296ad537fcb91eece4e50c9ff8d69a95
Jeff Malone <jeff@feb22>
2024-05-06 10:58:28 +0200

    auto commit 2024-05-06T10:58:28 CEST

--- bin/hosts-stdin.sh
+++ bin/hosts-stdin.sh
@@ -6,6 +6,7 @@ umask 027
 export PATH=/usr/local/sbin:/sbin:/bin:/usr/sbin:/usr/bin:~/bin
 
 cat - |
+    tr '[]' '[ *]' |
     sed -r -e 's/[ :,\t/]/\n/g' |
     tr '[[:upper:]]' '[[:lower:]]' |
     grep -E '^([a-z][a-z0-9-]{1,40}\.){2,5}(lan|co|io|com|ch|mgnt|local|il)(\.|\.yml)?$' |
