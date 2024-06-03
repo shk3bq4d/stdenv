97f6d08b30c455e89bdd65127b1cbc0a41d1609f
Jeff Malone <jeff@feb22>
2024-06-03 17:30:34 +0200

    auto commit 2024-06-03T17:30:34 CEST

--- bin/gc-reedit-message.sh
+++ bin/gc-reedit-message.sh
@@ -5,15 +5,32 @@ set -euo pipefail
 umask 027
 export PATH=/usr/local/sbin:/sbin:/usr/local/bin:/bin:/usr/sbin:/usr/bin:~/bin
 
-if [[ $# -eq 0 ]]; then
-    ref=HEAD
-elif [[ "$1" == "-"* ]]; then
-    ref=HEAD
-elif git check-ref-format "$1" &>/dev/null; then
-    ref="$1"
-    shift
-else
-    ref="$(git log -1 --pretty='%H' "$@")"
-fi
+args=""
+ref=""
+while :; do
+    if [[ $# -eq 0 ]]; then
+        if [[ -z "$ref" ]]; then
+            ref=HEAD
+        fi
+        break
+    fi
 
-git commit --reset-author --reedit-message=$ref "$@"
+    if [[ "$1" == "-"* ]]; then
+        args="$args $1"
+        shift
+        continue
+    fi
+
+    if git check-ref-format "$1" &>/dev/null; then
+        ref="$1"
+        shift
+        continue
+    fi
+    if [[ -z "$ref" ]]; then
+        ref="$(git log -1 --pretty='%H' "$@")"
+        shift
+    fi
+    break
+done
+
+git commit --reset-author --reedit-message=$ref $args "$@"
