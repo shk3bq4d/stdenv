5bb1f2b9d8b00182f454b7bc73d6a4232055a67b
Jeff Malone <jeff@nov20>
2024-09-01 11:37:39 +0200

    auto commit 2024-09-01T11:37:39 CEST

---
+++ bin/docker-python2-x11.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+# ex: set filetype=sh fenc=utf-8 expandtab ts=4 sw=4 :
+
+set -euo pipefail
+umask 027
+export PATH=/usr/local/sbin:/sbin:/usr/local/bin:/bin:/usr/sbin:/usr/bin:~/bin
+
+args() {
+    if [[ $# -eq 0 ]]; then
+        return
+    fi
+    if [[ $# -eq 1 ]]; then
+        case "$1" in \
+        bash) echo bash; return;;
+        esac
+    fi
+    echo "$@"
+}
+
+docker ps &>/dev/null && SUDO="" || SUDO="sudo";
+
+test -z "${DISPLAY:-}" && DISPLAY=":0"
+
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+chmod 0755 $XAUTH
+
+$SUDO docker run \
+    --rm  \
+    -it \
+    -e DISPLAY=${DISPLAY} \
+    -v $XSOCK:$XSOCK \
+    -v $XAUTH:$XAUTH \
+    -v $HOME:$HOME \
+    -w $PWD \
+    -u $(id -u):$(id -g) \
+    -e XAUTHORITY=$XAUTH \
+    python:2.7 \
+    $(args "$@")
+
+echo EOF
+exit 0
