6067607e96639f3fdba9f3498a48987e12799e11
Jeff Malone <jeff@may19>
2024-09-27 09:13:47 +0200

    auto commit fre 27 sep 2024 09:13:47 CEST

--- bin/rsync-remote-root.sh
+++ bin/rsync-remote-root.sh
@@ -1,34 +1,7 @@
 #!/usr/bin/env bash
 # ex: set filetype=sh :
-##
-##Usage:  __SCRIPT__ REMOTEHOST [REMOTEPORT]
-##configures whatever action with whatever config
-##    REMOTEHOST: remote host where to ssh
-##    REMOTEPORT: JMX port (default: 12345)
-##
-## Author: Jeff Malone, 24 Sep 2018
-##
 
 set -euo pipefail
 
-# function usage() { sed -r -n -e s/__SCRIPT__/$(basename $0)/ -e '/^##/s/^..// p'   $0 ; }
-
-# [[ $# -eq 1 && ( $1 == -h || $1 == --help ) ]] && usage && exit 0
-
-# [[ $# -lt 1 || $# -gt 2 ]] && echo FATAL: incorrect number of args && usage && exit 1
-
-# for i in sed which grep; do ! command -v $i &>/dev/null && echo FATAL: unexisting dependency $i && exit 1; done
-
-# DIR="$( cd -P "$( dirname "$(readlink -f "${BASH_SOURCE[0]}")" )" && pwd )"
-
-# exec > >(tee -a ~/log/$(basename $0 .sh).log)
-# exec > >(tee >(logger --id=$$ -t "$(basename $0)" -p user.info ))
-# exec 2>&1
-
-# test -z "${HOSTNAMEF:-}" && HOSTNAMEF=$(hostname -f)
-
-#if [[ -z ${SUDO_USER:-} ]]; then
-	rsync --rsh="ssh -t" --rsync-path="sudo rsync" "$@"
-#else
-#	rsync --rsh="ssh -F $(eval echo -n ~$SUDO_USER)/.ssh/config" --rsync-path="sudo rsync" "$@"
-#fi
+set -x
+rsync --rsh="ssh -t" --rsync-path="sudo nice -n +3 rsync" "$@"
