d52788f528c9ee9bbefa62804eded8753c90f203
Jeff Malone <jeff@nov20>
2024-10-07 08:07:38 +0200

    auto commit Mon Oct  7 08:07:38 AM CEST 2024

--- bin/citrix-stop-kill-all.sh
+++ bin/citrix-stop-kill-all.sh
@@ -8,7 +8,13 @@ export PATH=/usr/local/sbin:/sbin:/usr/local/bin:/bin:/usr/sbin:/usr/bin:~/bin
 set +e
 set -x
 sudo systemctl stop ctxlogd
+sleep 1
 pkill ServiceRecord
 pkill wfica
+sleep 1
 sudo pkill ctxcwalogd
+sleep 1
+pkill -9 ServiceRecord
+pkill -9 wfica
+sudo pkill -9 ctxcwalogd
 exit 0
