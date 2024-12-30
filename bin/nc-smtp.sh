9b7aabd6021b90d26fa4b9674d745fb77f751a45
Jeff Malone <jeff@feb22>
2024-12-30 12:38:19 +0100

    auto commit Mon Dec 30 12:38:19 PM CET 2024

--- bin/nc-smtp.sh
+++ bin/nc-smtp.sh
@@ -22,15 +22,15 @@ p() {
     esac
 }
 
-SERVER="$(p "mail.mydomain.local" "what SMTP to connect to")"
-PORT="$(p "25" "what destination TCP port to use")"
-STARTTLS="$(p "no" "would you like to starttls ?")"
+[[ $# -eq 0 ]] && SERVER="$(p "mail.mydomain.local" "what SMTP to connect to")" || { SERVER="$1"; shift; }
+[[ $# -eq 0 ]] && PORT="$(p "25" "what destination TCP port to use")" || { PORT="$1"; shift; }
+[[ $# -eq 0 ]] && STARTTLS="$(p "no" "would you like to starttls ?")" || { STARTTLS="$1"; shift; }
 
-FROM_DOMAIN="$(p "$(hostname -f)" "what HELO value to use")"
-FROM_MAIL="$(p "$(id -un)@$(hostname -f)" "What from value to use")"
+[[ $# -eq 0 ]] && FROM_DOMAIN="$(p "$(hostname -f)" "what HELO value to use")" || { FROM_DOMAIN="$1"; shift; }
+[[ $# -eq 0 ]] && FROM_MAIL="$(p "$(id -un)@$(hostname -f)" "What from value to use")" || { FROM_MAIL="$1"; shift; }
 TO="test4324@mailinator.com"
 TO="$(id -un)@$(hostname -f)"
-TO="$(p "${GIT_AUTHOR_EMAIL:-$TO}" "Who to send to")"
+[[ $# -eq 0 ]] && TO="$(p "${GIT_AUTHOR_EMAIL:-$TO}" "Who to send to")" || { TO="$1"; shift; }
 SUBJECT="Test #${SESS} sent from $(hostname -f) on $(date +'%Y.%m.%d %H:%M:%S')"
 
 
@@ -68,6 +68,8 @@ hoho
 QUIT
 " > $_tempfile
 cat $_tempfile
+echo "To repeat your args use"
+echo "$0 \"$SERVER\" \"$PORT\" \"$STARTTLS\" \"$FROM_DOMAIN\" \"$FROM_MAIL\" \"$TO\""
 cat $_tempfile | while read line; do
     sleep 1
     echo $line >&2
