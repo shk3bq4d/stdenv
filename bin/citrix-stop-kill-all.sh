27aad0fcaad2d742f268094154666af8b53826d1
Jeff Malone <jeff@nov20>
2025-01-11 21:17:20 +0100

    auto commit Sat Jan 11 09:17:20 PM CET 2025

--- bin/citrix-stop-kill-all.sh
+++ bin/citrix-stop-kill-all.sh
@@ -8,10 +8,14 @@ export PATH=/usr/local/sbin:/sbin:/usr/local/bin:/bin:/usr/sbin:/usr/bin:~/bin
 set +e
 set -x
 sudo systemctl stop ctxlogd
+sudo systemctl stop ctxcwalogd
+# /opt/Citrix/ICAClient//UtilDaemon                                                                                                        13:24:58  45ms
 sleep 1
+sudo pkill -f ^/opt/Citrix/
 pkill ServiceRecord
 pkill wfica
 sleep 1
+sudo pkill -9 -f ^/opt/Citrix/
 sudo pkill ctxcwalogd
 sleep 1
 pkill -9 ServiceRecord
