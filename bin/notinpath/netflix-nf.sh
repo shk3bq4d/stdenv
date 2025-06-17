34c53146c50f7831b7aed620fe787375c0b6b9a5
Jeff Malone <jeff@nov20>
2025-06-17 09:38:25 +0200

    auto commit Tue Jun 17 09:38:25 AM CEST 2025

--- bin/notinpath/netflix-nf.sh
+++ bin/notinpath/netflix-nf.sh
@@ -2,6 +2,7 @@
 # /* ex: set filetype=sh fenc=utf-8 expandtab ts=4 sw=4 : */
 ~/bin/mute-off.sh
 ~/bin/volume-50.sh
+bt-airpod-marc-on.sh
 #nohup /usr/bin/google-chrome https://www.netflix.com/browse &>/dev/null &
 sites() {
     cat << EOF
@@ -10,8 +11,8 @@ EOF
 }
 for i in $(sites); do
     [[ -z "$i" ]] && continue
-    #nohup $HOME/bin/default-browser --new-window https://$i </dev/null &>/dev/null &
-    nohup $HOME/bin/chromium-browser --new-window https://$i </dev/null &>/dev/null &
+    nohup $HOME/bin/default-browser --new-window https://$i </dev/null &>/dev/null &
+    #nohup $HOME/bin/chromium-browser --new-window https://$i </dev/null &>/dev/null &
 done
 for i in $(seq 1); do
     #/usr/bin/mplayer ~/Music/bip-stereo.mp3
