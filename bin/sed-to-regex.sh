f354eb31bfede606a43aef5ff82e99efcfb35b36
Jeff Malone <jeff@may19>
2025-12-15 13:08:18 +0100

    auto commit Mon Dec 15 01:08:18 PM CET 2025

---
+++ bin/sed-to-regex.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# ex: set filetype=sh fenc=utf-8 expandtab ts=4 sw=4 :
+
+set -euo pipefail
+# date '+%a %b %d %H:%M' -d @1747872031
+umask 027
+export PATH=/usr/local/sbin:/sbin:/usr/local/bin:/bin:/usr/sbin:/usr/bin:~/bin
+export PS4='+ ${BASH_SOURCE:-}:${LINENO:-}:${FUNCNAME[0]:-}: ';
+
+echo -n '('
+cat "$@" | tr '\n' '|' | sed -r -e 's/.$//'
+echo -n ')'
+echo
