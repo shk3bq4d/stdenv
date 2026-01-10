d845f6d3df15c20772a1f2513046bab437888c6c
Jeff Malone <jeff@feb22>
2026-01-10 12:25:56 +0100

    auto commit Sat Jan 10 12:25:56 PM CET 2026

--- bin/clock.sh
+++ bin/clock.sh
@@ -8,19 +8,34 @@ export PATH=/usr/local/sbin:/sbin:/usr/local/bin:/bin:/usr/sbin:/usr/bin:~/bin
 if tput civis &>/dev/null; then # ignores TERM and lack of stdin problems
     trap "tput cnorm; echo; exit" INT
 fi
-host="$(hostname -f)"
+host="$(hostname -s)"
 if [[ $# -gt 0 ]]; then
     host="${host} $@"
 fi
 
 prev_len=0
+clear_count=0
+clear_count_threshold=30
+title_count_threshold=1
+title_count=$(( title_count_threshold + 1 ))
 
 while true; do
     time_str="${host} $(date +"%Y.%m.%d %H:%M:%S")"
     len=${#time_str}
 
-    # Erase previous characters if the new string is shorter
-    if (( len < prev_len )); then
+    title_count=$(( title_count + 1 ))
+    if (( title_count >= title_count_threshold )); then
+        title_count=0
+        echo -ne "\033]0;$time_str\007"
+    fi
+
+    clear_count=$(( clear_count + 1 ))
+    if (( clear_count >= clear_count_threshold )); then
+        clear_count=0
+        clear
+        echo -ne "\r$time_str"
+    elif (( len < prev_len )); then
+        # Erase previous characters if the new string is shorter
         diff=$((prev_len - len))
         padding=$(printf '%*s' "$diff" '')
         echo -ne "\r$time_str$padding"
