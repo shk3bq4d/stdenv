3ac4f3b9657777fb7edf393b47b929fbfb410560
Jeff Malone <jeff@feb22>
2026-01-10 13:03:46 +0100

    auto commit Sat Jan 10 01:03:46 PM CET 2026

--- bin/clock.sh
+++ bin/clock.sh
@@ -3,7 +3,11 @@
 
 set -euo pipefail
 umask 027
-export PATH=/usr/local/sbin:/sbin:/usr/local/bin:/bin:/usr/sbin:/usr/bin:~/bin
+export PATH=/usr/local/sbin:/sbin:/usr/local/bin:/bin:/usr/sbin:/usr/bin
+
+if [[ -z "${TERM:-}" ]]; then
+    export TERM=xterm # needed for clear
+fi
 
 if tput civis &>/dev/null; then # ignores TERM and lack of stdin problems
     trap "tput cnorm; echo; exit" INT
@@ -16,16 +20,17 @@ fi
 prev_len=0
 clear_count=0
 clear_count_threshold=30
-title_count_threshold=1
-title_count=$(( title_count_threshold + 1 ))
+title_count_threshold=60
+title_count="$(date +%S)"
+_sleep=1
 
 while true; do
     time_str="${host} $(date +"%Y.%m.%d %H:%M:%S")"
     len=${#time_str}
 
     title_count=$(( title_count + 1 ))
-    if (( title_count >= title_count_threshold )); then
-        title_count=0
+    if (( title_count > title_count_threshold )); then
+        title_count="$(( $(date +%S) - $_sleep ))"
         echo -ne "\033]0;$time_str\007"
     fi
 
@@ -44,5 +49,5 @@ while true; do
     fi
 
     prev_len=$len
-    sleep 1
+    sleep $_sleep
 done
