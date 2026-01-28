5faa2dec7602a0425375c2538ece195a502736a1
Jeff Malone <jeff@shaz0140500>
2026-01-28 16:49:38 +0100

    auto commit Wed Jan 28 04:49:38 PM CET 2026

--- bin/rsync-remote-root.sh
+++ bin/rsync-remote-root.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 
 set -f # disable path expansion globbing *
 set -x
-rsync --rsh="ssh -t" --rsync-path="sudo nice -n +3 rsync" "$@"
+rsync --rsh="ssh -t -l automation" --rsync-path="sudo nice -n +3 rsync" "$@"
