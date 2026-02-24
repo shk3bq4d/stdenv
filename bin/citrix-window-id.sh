672662832db1bc9d2a27b4afeb0295dd1156f79f
Jeff Malone <jeff@feb22>
2026-02-24 12:02:29 +0100

    auto commit Tue Feb 24 12:02:29 PM CET 2026

--- bin/citrix-window-id.sh
+++ bin/citrix-window-id.sh
@@ -10,7 +10,7 @@ if [[ -n "${CITRIX_WINDOW_ID_OVERRIDE:-}" ]]; then
 fi
 
 if true; then
-    xdotool search --name "Windows 11 - ITTS|ITTS Desktop" > $_tempfile
+    xdotool search --name "Windows 11 - ITTS|ITTS Desktop|Windows 11 - External" > $_tempfile
     xdotool search --class Wfica | grep -f $_tempfile
 else
     xdotool search --name "zscaler-ubuntu20-citrix-gnome_shaz0140003" > $_tempfile
