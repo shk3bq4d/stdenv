3a683ca29aaa7a16a1c1b100cca6359652cbe164
Jeff Malone <jeff@shaz0140504>
2026-06-25 09:24:11 +0200

    auto commit Thu Jun 25 09:24:11 AM CEST 2026

--- bin/ansible-hosts-list.sh
+++ bin/ansible-hosts-list.sh
@@ -6,7 +6,15 @@ umask 027
 export PATH=/usr/local/sbin:/sbin:/bin:/usr/sbin:/usr/bin:~/bin
 
 if [[ $# -eq 0 ]]; then
-    ARGS=all
+    if false && [[ -t 0 ]]; then
+        ARGS=all
+    else
+        ARGS="$(hosts-from-clipboard.sh)"
+        if [[ "$(echo "$ARGS" | wc -l)" -eq 0 ]]; then
+            >&2 echo "enter your hosts on the stdin"
+            ARGS="$(hosts-stdin.sh)"
+        fi
+    fi
 else
     ARGS="$@"
 fi
