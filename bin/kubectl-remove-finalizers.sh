1c54fa550925e469803f63acc981e27046cbc971
Jeff Malone <jeff@shaz0140504>
2026-09-04 17:20:38 +0200

    auto commit Fri Sep  4 05:20:38 PM CEST 2026

---
+++ bin/kubectl-remove-finalizers.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# ex: set filetype=sh fenc=utf-8 expandtab ts=4 sw=4 :
+
+set -Eeuo pipefail
+shopt -s inherit_errexit
+umask 027
+export PATH=/usr/local/sbin:/sbin:/usr/local/bin:/bin:/usr/sbin:/usr/bin:~/bin
+export PS4='+ ${BASH_SOURCE:-}:${LINENO:-}:${FUNCNAME[0]:-}: ';
+
+set -x
+kubectl patch "$@" --type=merge -p '{"metadata":{"finalizers":[]}}'
